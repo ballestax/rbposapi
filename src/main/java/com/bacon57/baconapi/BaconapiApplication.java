ae0dd41fa673ce338b08b8a9a6690a10c712e2df
LuisR <ballestax@gmail.com>
2022-08-09 15:11:16 -0500

    First commit

---
+++ src/main/java/com/bacon57/baconapi/BaconapiApplication.java
@@ -0,0 +1,13 @@
+package com.bacon57.baconapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaconapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaconapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bacon57/baconapi/BaconapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bacon57.baconapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaconapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
